c7ed2b6c193edcf20961b266e6ad77487dcb3ef2
Alfredo Pizarro <4797360-apizarro@users.noreply.gitlab.com>
2020-07-19 03:03:15 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Santiago');
 $hostname = gethostname();
-$date = date('d/m/Y h:i:s a', time());
+$date = date('d-m-Y h:i:s a', time());
 $myObj = new stdClass();
 $myObj->pod = $hostname;
 $myObj->fecha = $date;
