61ce6ae4d9409b31fc6deeaa471b7730032f303d
Alfredo Pizarro <50894645+AlfredoPizarro@users.noreply.github.com>
2020-11-16 14:55:37 -0300

    Create app.php

---
+++ app.php
@@ -0,0 +1,4 @@
+<!-- #######  THIS IS A COMMENT - Visible only in the source editor #########-->
+<h2 style="text-align: center;">P&aacute;gina de prueba</h2>
+<p>P&aacute;gina de prueba Openshift test1</p>
+<p>&nbsp;</p>
